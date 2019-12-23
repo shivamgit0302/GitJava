edbfbff336edffc808ca79881fafe05665243667
shivamgit0302 <59158846+shivamgit0302@users.noreply.github.com>
2019-12-23 11:39:04 +0530

    Add files via upload

---
+++ PerfectNumber.java
@@ -0,0 +1,15 @@
+class PerfectNumber
+{
+public static void main(String arg[])
+{
+for(int i=0;i<1001;i++)
+{
+int sum=0;
+for(int j=1;j<i;j++)
+if(i%j==0)
+sum=sum+j;
+if(sum==i && sum!=0)
+System.out.print(i+ "\t");
+}
+}
+}
